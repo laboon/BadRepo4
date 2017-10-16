6eee0562bd777ebb60d8a66ee576a6176057528b
benkristofic <ben.kristofic@gmail.com>
2017-10-16 16:03:56 -0400

    clean code

--- com/laboon/Cell.java
+++ com/laboon/Cell.java
@@ -65,15 +65,13 @@ public class Cell {
 	 */
 	
 	public char getStateRep() {
-		char toReturn = ' ';
-		if (_state == State.DEAD) {
-			toReturn = ' ';
-		} else if (_state == State.ALIVE) {
-			toReturn = '*';
-		} else {
-			toReturn = ' ';
-		}
-		return toReturn;
+	char toReturn = ' ';
+	if (_state == State.DEAD) {
+	    toReturn = '.';
+	} else if (_state == State.ALIVE) {
+	    toReturn = '*';
+	} else {
+	    toReturn = ' ';
 	}
 	
 	/**
